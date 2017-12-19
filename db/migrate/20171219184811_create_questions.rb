84bb98d4f5f089c2b23ac35c083dc7cdaa99d0ad
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-19 12:48:50 -0600

    Scaffold in a question model

---
+++ db/migrate/20171219184811_create_questions.rb
@@ -0,0 +1,14 @@
+class CreateQuestions < ActiveRecord::Migration[5.1]
+  def change
+    create_table :questions do |t|
+      t.string :content
+      t.integer :yes
+      t.integer :no
+      t.integer :unsure
+      t.string :comments
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
