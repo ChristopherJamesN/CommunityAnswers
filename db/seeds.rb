ecd3f5aa035c4e207eff1d50dc2d8bc04645ad52
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-19 15:44:54 -0600

    Add seed questions to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Question.create(content: 'Should I buy a new iPhone?')
+Question.create(content: 'Should I get my 8 year old daughter a dog?')
+Question.create(content: 'Is $20 per month a reasonable allowance for an 11 year old boy?')
+Question.create(content: 'Is it worth it to upgrade my internet speed from 15 mbps to 30 mbps for $20 more per month?')
+Question.create(content: 'Is living in downtown San Francisco worth the cost of living?')
